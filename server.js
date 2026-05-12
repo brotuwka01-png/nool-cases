a8e4e5b8050859c6a03cdff7606f4b1a132f8297
brotuwka01-png <brotuwka01@gmail.com>
2026-05-12 15:24:33 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const TelegramBot = require("node-telegram-bot-api")
+const path = require("path")
 
 const app = express()
 
@@ -7,40 +8,7 @@ const TOKEN = process.env.BOT_TOKEN
 
 const bot = new TelegramBot(TOKEN, { polling: true })
 
-const cases = [
-{
-name: "🐻 Медведь",
-chance: 5
-},
-{
-name: "❤️ Сердце",
-chance: 5
-},
-{
-name: "❌ Пусто",
-chance: 90
-}
-]
-
-function rollCase() {
-
-const random = Math.random() * 100
-
-let current = 0
-
-for (const item of cases) {
-
-current += item.chance
-
-if (random <= current) {
-return item.name
-}
-
-}
-
-return "❌ Пусто"
-
-}
+app.use(express.static(__dirname))
 
 bot.onText(/\/start/, (msg) => {
 
@@ -48,81 +16,29 @@ bot.sendMessage(msg.chat.id,
 
 `🎰 NOOL CASES
 
-💎 Открывай кейсы за Telegram Stars
+🔥 Mini App готов
 
-🔥 Редкие призы
-🐻 NFT
-❤️ Подарки
-🎁 И многое другое`,
+👇 Нажми кнопку снизу`,
 {
 reply_markup: {
 keyboard: [
-["🎰 Открыть кейс"],
-["👤 Профиль"]
+[
+{
+text:"🎰 OPEN CASES",
+web_app:{
+url:"https://nool-cases.onrender.com"
+}
+}
+]
 ],
-resize_keyboard: true
+resize_keyboard:true
 }
 })
-
-})
-
-bot.on("message", async (msg) => {
-
-if (!msg.text) return
-
-if (msg.text === "🎰 Открыть кейс") {
-
-bot.sendMessage(msg.chat.id,
-
-`💫 CASE PRICE: 15 ⭐
-
-Для теста кейс откроется бесплатно`
-)
-
-bot.sendMessage(msg.chat.id, "🎰 Крутится рулетка...")
-
-setTimeout(() => {
-
-const win = rollCase()
-
-bot.sendMessage(msg.chat.id,
-
-`🎉 Результат:
-
-${win}`
-)
-
-}, 3000)
-
-}
-
-if (msg.text === "👤 Профиль") {
-
-bot.sendMessage(msg.chat.id,
-
-`👤 Ваш профиль
-
-🆔 ID:
-${msg.from.id}
-
-⭐ Кейсов открыто:
-0`
-)
-
-}
 
 })
 
 app.get("/", (req,res) => {
-
-res.send(`
-
-<h1 style="font-family:sans-serif;">
-NOOL CASES ONLINE 🎰
-</h1>
-
-`)
-
+res.sendFile(path.join(__dirname,"index.html"))
 })
 
 app.listen(3000, () => {
